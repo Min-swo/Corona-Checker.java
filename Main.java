014dbf76b3a5e93e3b6f75b316dcc2ac90a54640
Min-swo <msms0226@g.skku.edu>
2021-05-28 14:18:10 +0900

    defined PrsInfo & InsInfo

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -6,8 +11,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		List list = new List();
-		//list.addTo_iList(makeInstitutionInfo());
+		getPersonInfo(list);
+		list.addTo_iList(makeInstitutionInfo("Alchon", "433-119, Yuljeon-dong"));
 		
+		list.showpList();
+		list.showInstitution();
+
 		
 		//Scanner sc = new Scanner(System.in);
 		//String input;
@@ -33,15 +42,38 @@ public class Main {
 	}
 	//**NOT DEFINED** gui 격리 해제 버튼을 통해 List의 quitIsolation() 메소드를 호출하는 메소드 필요
 	
-	//**NOT DEFINED** file 파일을 불러와 makePersonInfo에 정보를 넘겨주는 메소드 필요
-	
+	//**DEFINING** file 파일을 불러와 makePersonInfo에 정보를 넘겨주는 메소드 필요
+	public static void getPersonInfo(List list)
+	{
+		String name = "";
+		String phone = "";
+		int infectionStatus = 0;
+		try{
+	         File file = new File("./src/Sample.txt"); // File 이름 필요
+	         FileReader filereader = new FileReader(file);
+	         BufferedReader bufReader = new BufferedReader(filereader);
+	         String line = "";
+	         while((line = bufReader.readLine()) != null){
+	             name = line.split(" ")[0];
+	             phone = line.split(" ")[1];
+	             infectionStatus = Integer.parseInt(line.split(" ")[2]);
+	             list.addTo_pList(makePersonInfo(name, phone, infectionStatus));
+	             //System.out.print(name + " "+ phone + "\n");
+	         }
+	         bufReader.close();
+		 }catch (FileNotFoundException e) {
+	         // TODO: handle exception
+		 }catch(IOException e){
+	         System.out.println(e);
+	     }
+	}
 	public static Person makePersonInfo(String name, String phone, int infectionStatus){
 		Person p = new Person(name, phone, infectionStatus);
 		return p;
 	}
-	//**NOT DEFINED** file 파일을 불러와 Institution에 정보를 넘겨주는 메소드 필요
+	//**DEFINING** file 파일을 불러와 Institution에 정보를 넘겨주는 메소드 필요
 	
-	public static Institution makeInstitutionInfo(String name, String address, ArrayList<pList_Client> pc) { 
+	public static Institution makeInstitutionInfo(String name, String address) { 
 		//DateTime temptime1 = new DateTime(2021,5,22,10,46);
 		//pList_Client client1 = new pList_Client("우세진", "010-9858-9060", 0, temptime1);
 		
@@ -57,6 +89,40 @@ public class Main {
 		//al.add(client3);
 		
 		//Institution i = new Institution("본찌돈까스", " 수원시 장안구 서부로2106번길 22", al);
+		ArrayList<pList_Client> pc = new ArrayList<pList_Client>();
+		String cName = "";
+		String cPhone = "";
+		int infectionStatus = 0;
+		String Date = new String();
+		int year; int month; int  day; int hour; int minute;
+		try{
+	         File file = new File("./src/" + name + ".txt");
+	         FileReader filereader = new FileReader(file);
+	         BufferedReader bufReader = new BufferedReader(filereader);
+	         String line = "";
+	         while((line = bufReader.readLine()) != null){
+	             cName = line.split(" ")[0];
+	             cPhone = line.split(" ")[1];
+	             infectionStatus = Integer.parseInt(line.split(" ")[2]);
+	             
+	             Date = line.split(" ")[3];
+	             year = Integer.parseInt(Date.split("\\.")[0]);
+	             month = Integer.parseInt(Date.split("\\.")[1]);
+	             day = Integer.parseInt(Date.split("\\.")[2]);
+	             hour = Integer.parseInt(Date.split("\\.")[3]);
+	             minute = Integer.parseInt(Date.split("\\.")[4]);
+	             
+	             DateTime time = new DateTime(year, month, day, hour, minute);
+	             pList_Client client = new pList_Client(cName, cPhone, infectionStatus, time);
+	             pc.add(client);
+	         }
+	         bufReader.close();
+		 }catch (FileNotFoundException e) {
+	         // TODO: handle exception
+		 }catch(IOException e){
+	         System.out.println(e);
+	     }
+
 		Institution i = new Institution(name, address, pc);
 		return i;
 	}
